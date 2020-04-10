6807f9b729c1c43eaf6af4d37e19639fbad286cc
Gokhan Torun <gkhn-90@hotmail.com>
2020-04-10 21:24:31 +0300

    .

--- angular-src/src/app/components/harf/harf.component.ts
+++ angular-src/src/app/components/harf/harf.component.ts
@@ -6,10 +6,6 @@ import { trigger,state, transition, animate, style } from "@angular/animations";
   templateUrl: "./harf.component.html",
   styleUrls: ["./harf.component.scss"],
   animations: [
-    // trigger("flyAnimations", [
-    //   transition(":enter", [style({ opacity: 0, }),animate("800ms", style({ opacity: 1 }))]),
-    //   transition(":leave", [animate("800ms", style({ opacity: 0 }))])
-    // ])
     trigger("rotatedState", [
        transition(":enter", [style({ transform: 'rotateX(0)' }),animate("200ms ease-out", style({ transform: 'rotateX(360deg)' }))]),
        transition(":leave", [animate("200ms ease-in", style({ transform: 'rotateX(360deg)' }))])
